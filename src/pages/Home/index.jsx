f12c817890b2baac032f6c2f27d7fcdbdb1278c9
manu-drk <manu.drk.06@outlook.com>
2024-04-23 12:13:25 +0200

    up

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/'
 import Survey from './pages/Survey/'
-import Header from './components/Header'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey" element={<Survey />} />
